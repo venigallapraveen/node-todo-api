695590d55882d9af89b6c68ab632b5c128cd85f3
Praveen Venigalla <praveenv.aiesec@gmail.com>
2018-06-21 19:41:26 -0400

    add post todos and refractor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+
+var Todo = mongoose.model('Todo',{
+    text :{
+        type: String,
+        required : true,
+        minlength : 1,
+        trim : true
+    },
+    completed:{
+        type : Boolean
+    },
+    completedAt :{
+        type : Number
+    }
+
+});
+
+module.exports = {
+    Todo
+}
